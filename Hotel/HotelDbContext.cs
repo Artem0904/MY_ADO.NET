794c5696a12dd0f709bdfb567e8f6cef7cfe9d8c
Artem0904 <moonportal708@gmail.com>
2023-08-28 15:50:01 +0300

    start hotel

---
+++ Hotel/HotelDbContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel
+{
+    public class Position
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+
+
+
+    internal class HotelDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            //optionsBuilder.UseLazyLoadingProxies();
+
+            string conn = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=HotelDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False;MultipleActiveResultSets=true";
+            optionsBuilder.UseSqlServer(conn);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // ---------------- FluentAPI Configurations
+            //modelBuilder.ApplyConfiguration(new EmployeeConfigs());
+
+            // data initialization
+            //DbExtensions.SeedData(modelBuilder);
+            //modelBuilder.SeedData();
+        }
+
+        // Object Collections (Tables in SQL)
+        //public DbSet<Employee> Employees { get; set; }
+        //public DbSet<Position> Positions { get; set; }
+        //public DbSet<Order> Orders { get; set; }
+        //public DbSet<Dish> Dishes { get; set; }
+        //public DbSet<Resume> Resumes { get; set; }
+    }
+}
